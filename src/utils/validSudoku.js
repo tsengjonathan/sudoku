74e472b6d98b966d2f27a63a0b60f16db4713cf2
tsengjonathan <tseng.j@husky.neu.edu>
2019-10-21 07:55:56 -0400

    :art: Adjust validSudokuArray

--- src/utils/validSudoku.js
+++ src/utils/validSudoku.js
@@ -4,16 +4,20 @@ import _ from 'lodash';
  * Checks if the given array is a valid Sudoku array.
  * A valid Sudoku array is defined as an array with non-repeating values, 1 to 9 inclusive
  * Used to check if a given row/column/square of sudoku numbers are valid
- * @param {number[]} set array of numbers to check
+ * @param {*} set arbitrary input to check for a valid sudoku array
+ * @returns {boolean} whether the input is a valid sudoku array or not
  */
 function validSudokuArray(set) {
-  if (!Object.prototype.hasOwnProperty.call(set, 'length')) {
-    throw Error(`validSudokurray expected an Array - received ${typeof set} instead`);
-  } else if (set.length !== 9) {
-    throw Error(`validSudokuArray expected an Array of length 9 - received ${set.length} instead`);
+  if (!set || !Object.prototype.hasOwnProperty.call(set, 'length') || set.length !== 9) {
+    return false;
   }
+
   const validNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   return _.xor(set, validNumbers).length === 0;
 }
 
 export default validSudokuArray;
+
+export {
+  validSudokuArray,
+};
