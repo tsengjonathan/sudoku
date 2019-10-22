5c4164e768169079182306ee79ba0a3239b033e5
tsengjonathan <tseng.j@husky.neu.edu>
2019-10-22 19:13:25 -0400

    Updates

---
+++ precache-manifest.2bfe31b3ed1ba3a9eea54ab0a7a19745.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3585cb998c88b4ed2446a5af5f14f21f",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "e52fde0fd7ba3a9a7af5",
+    "url": "/sudoku/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "119f292f8a1876eb2974",
+    "url": "/sudoku/static/js/2.ba79ab60.chunk.js"
+  },
+  {
+    "revision": "e52fde0fd7ba3a9a7af5",
+    "url": "/sudoku/static/js/main.ee72ef97.chunk.js"
+  },
+  {
+    "revision": "c62683295d68da0e2a26",
+    "url": "/sudoku/static/js/runtime-main.9d6158fb.js"
+  }
+]);
