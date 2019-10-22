b9f37ced44db1df5fb1a5ad8f6d53dea24e3a778
tsengjonathan <tseng.j@husky.neu.edu>
2019-10-21 21:48:32 -0400

    :white_check_mark: Structure validSudokuArray Tests

--- src/utils/tests/validSudoku.test.js
+++ src/utils/tests/validSudoku.test.js
@@ -2,30 +2,32 @@ import _ from 'lodash';
 
 import { validSudokuArray } from '../validSudoku';
 
-test('validSudokuArray - empty array', () => {
-  expect(validSudokuArray([])).toBe(false);
-});
+describe('validSudokuArray', () => {
+  test('rejects an empty array', () => {
+    expect(validSudokuArray([])).toBe(false);
+  });
 
-test('validSudokuArray - invalid object types', () => {
-  expect(validSudokuArray(0)).toBe(false);
-  expect(validSudokuArray(null)).toBe(false);
-  expect(validSudokuArray(undefined)).toBe(false);
-});
+  test('rejects invalid object types', () => {
+    expect(validSudokuArray(0)).toBe(false);
+    expect(validSudokuArray(null)).toBe(false);
+    expect(validSudokuArray(undefined)).toBe(false);
+  });
 
-test('validSudokuArray - valid array', () => {
-  const orderedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const unorderedArray = _.shuffle(orderedArray);
+  test('accepts valid array', () => {
+    const orderedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const unorderedArray = _.shuffle(orderedArray);
 
-  expect(validSudokuArray(orderedArray)).toBe(true);
-  expect(validSudokuArray(unorderedArray)).toBe(true);
-});
+    expect(validSudokuArray(orderedArray)).toBe(true);
+    expect(validSudokuArray(unorderedArray)).toBe(true);
+  });
 
-test('validSudokuArray - invalid array', () => {
-  const shortArray = [1, 2, 3, 4, 5];
-  const longArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  const duplicateArray = [1, 1, 1, 1, 1, 1, 1, 1, 1];
+  test('rejects invalid arrays', () => {
+    const shortArray = [1, 2, 3, 4, 5];
+    const longArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    const duplicateArray = [1, 1, 1, 1, 1, 1, 1, 1, 1];
 
-  expect(validSudokuArray(shortArray)).toBe(false);
-  expect(validSudokuArray(longArray)).toBe(false);
-  expect(validSudokuArray(duplicateArray)).toBe(false);
+    expect(validSudokuArray(shortArray)).toBe(false);
+    expect(validSudokuArray(longArray)).toBe(false);
+    expect(validSudokuArray(duplicateArray)).toBe(false);
+  });
 });
