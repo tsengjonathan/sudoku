1f053becfaa06ba3db65486a4bd69e93434046a1
tsengjonathan <tsengjonathan@users.noreply.github.com>
2019-10-23 03:52:55 +0000

    Automated deployment: Wed Oct 23 03:52:55 UTC 2019 e889cc7e6816df48b32cadede5a7aa1f07a4d458

---
+++ precache-manifest.6ec722587ca96784354da207262d1ef0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063a83e38bb8bb0493df3b473b46f2ec",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "5991f6ed5a83e244ab78",
+    "url": "/sudoku/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "119f292f8a1876eb2974",
+    "url": "/sudoku/static/js/2.ba79ab60.chunk.js"
+  },
+  {
+    "revision": "5991f6ed5a83e244ab78",
+    "url": "/sudoku/static/js/main.45ff77de.chunk.js"
+  },
+  {
+    "revision": "c62683295d68da0e2a26",
+    "url": "/sudoku/static/js/runtime-main.9d6158fb.js"
+  }
+]);
