3741dad4a7d142e0cf72f9e98e8bc1feda2f4242
tsengjonathan <tseng.j@husky.neu.edu>
2019-10-22 21:04:33 -0400

    :pencil: Add JSDoc for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
 
+/**
+ * Parent wrapper for all React code
+ *
+ * @returns {object} App
+ */
 function App() {
   return (
     <div className="App">
