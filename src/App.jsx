680c63247a8e8fd4d2b3a36fde8803e18dff808a
tsengjonathan <tseng.j@husky.neu.edu>
2019-11-11 22:17:22 -0500

    :sparkles: Add Game to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import Game from './components/Game';
+
 /**
  * Parent wrapper for all React code
  *
@@ -9,7 +11,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header" />
+      <Game />
     </div>
   );
 }
