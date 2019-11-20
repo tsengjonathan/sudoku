cc134e369040e53f2ebd46c539f83600d61aef17
tsengjonathan <tseng.j@husky.neu.edu>
2019-11-20 07:49:16 -0500

    :sparkles: Add empty Sudoku

---
+++ src/puzzles/empty.js
@@ -0,0 +1,20 @@
+import { rowsToColumns, rowsToBlocks } from '../utils/adaptors';
+
+const sudoku = {
+  rows: [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ],
+};
+
+sudoku.columns = rowsToColumns(sudoku.rows);
+sudoku.blocks = rowsToBlocks(sudoku.rows);
+
+export default sudoku;
