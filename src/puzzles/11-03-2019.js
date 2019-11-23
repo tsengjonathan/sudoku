722c1daf9669a6ec46f699f32676dc0d574a95b6
tsengjonathan <tseng.j@husky.neu.edu>
2019-11-23 17:13:31 -0500

    :recycle: Refactor puzzles

--- src/puzzles/11-03-2019.js
+++ src/puzzles/11-03-2019.js
@@ -1,20 +1,15 @@
-import { rowsToColumns, rowsToBlocks } from '../utils/adaptors';
+import { prepareSudoku } from '../utils/adaptors';
 
-const sudoku = {
-  rows: [
-    [1, 0, 0, 7, 0, 9, 0, 0, 0],
-    [5, 8, 0, 0, 0, 0, 0, 3, 0],
-    [7, 0, 6, 0, 0, 0, 0, 1, 9],
-    [0, 7, 0, 0, 0, 3, 0, 0, 8],
-    [9, 0, 1, 0, 0, 0, 0, 0, 2],
-    [6, 0, 8, 5, 0, 0, 0, 0, 7],
-    [0, 0, 7, 0, 3, 0, 0, 0, 0],
-    [0, 0, 0, 9, 0, 0, 4, 0, 3],
-    [0, 0, 4, 0, 7, 6, 8, 5, 0],
-  ],
-};
+const sudoku = [
+  [1, 0, 0, 7, 0, 9, 0, 0, 0],
+  [5, 8, 0, 0, 0, 0, 0, 3, 0],
+  [7, 0, 6, 0, 0, 0, 0, 1, 9],
+  [0, 7, 0, 0, 0, 3, 0, 0, 8],
+  [9, 0, 1, 0, 0, 0, 0, 0, 2],
+  [6, 0, 8, 5, 0, 0, 0, 0, 7],
+  [0, 0, 7, 0, 3, 0, 0, 0, 0],
+  [0, 0, 0, 9, 0, 0, 4, 0, 3],
+  [0, 0, 4, 0, 7, 6, 8, 5, 0],
+];
 
-sudoku.columns = rowsToColumns(sudoku.rows);
-sudoku.blocks = rowsToBlocks(sudoku.rows);
-
-export default sudoku;
+export default prepareSudoku(sudoku);
--- src/puzzles/11-23-2019.js
+++ src/puzzles/11-23-2019.js
@@ -1,20 +1,15 @@
-import { rowsToColumns, rowsToBlocks } from '../utils/adaptors';
+import { prepareSudoku } from '../utils/adaptors';
 
-const sudoku = {
-  rows: [
-    [0, 0, 0, 0, 0, 1, 0, 8, 0],
-    [8, 1, 0, 6, 0, 0, 0, 0, 7],
-    [0, 0, 4, 0, 2, 0, 6, 0, 0],
-    [0, 6, 0, 2, 0, 8, 0, 0, 0],
-    [0, 0, 5, 0, 0, 0, 0, 0, 2],
-    [0, 0, 0, 7, 0, 3, 1, 9, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [4, 0, 1, 9, 0, 2, 0, 7, 8],
-    [0, 0, 6, 1, 0, 0, 0, 0, 9],
-  ],
-};
+const sudoku = [
+  [0, 0, 0, 0, 0, 1, 0, 8, 0],
+  [8, 1, 0, 6, 0, 0, 0, 0, 7],
+  [0, 0, 4, 0, 2, 0, 6, 0, 0],
+  [0, 6, 0, 2, 0, 8, 0, 0, 0],
+  [0, 0, 5, 0, 0, 0, 0, 0, 2],
+  [0, 0, 0, 7, 0, 3, 1, 9, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [4, 0, 1, 9, 0, 2, 0, 7, 8],
+  [0, 0, 6, 1, 0, 0, 0, 0, 9],
+];
 
-sudoku.columns = rowsToColumns(sudoku.rows);
-sudoku.blocks = rowsToBlocks(sudoku.rows);
-
-export default sudoku;
+export default prepareSudoku(sudoku);
