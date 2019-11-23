390324a50d50d59f82d76bb10a952be837ceac8f
tsengjonathan <tseng.j@husky.neu.edu>
2019-11-23 18:01:24 -0500

    :white_check_mark: Update extractors test
    Sudokus and puzzles have changed its structure, so the corresponding
     extractors have to be updated as well.

--- src/utils/tests/extractors.test.js
+++ src/utils/tests/extractors.test.js
@@ -3,31 +3,59 @@ import _ from 'lodash';
 import { getRowFromIndex, getColumnFromIndex, getBlockFromIndex } from '../extractors';
 import sudoku from '../../puzzles/11-03-2019';
 
+const row = [
+  { value: 1, fixed: true },
+  { value: 0, fixed: false },
+  { value: 0, fixed: false },
+  { value: 7, fixed: true },
+  { value: 0, fixed: false },
+  { value: 9, fixed: true },
+  { value: 0, fixed: false },
+  { value: 0, fixed: false },
+  { value: 0, fixed: false },
+];
+
+const column = [
+  { value: 1, fixed: true },
+  { value: 5, fixed: true },
+  { value: 7, fixed: true },
+  { value: 0, fixed: false },
+  { value: 9, fixed: true },
+  { value: 6, fixed: true },
+  { value: 0, fixed: false },
+  { value: 0, fixed: false },
+  { value: 0, fixed: false },
+];
+
+const block = [
+  { value: 1, fixed: true },
+  { value: 0, fixed: false },
+  { value: 0, fixed: false },
+  { value: 5, fixed: true },
+  { value: 8, fixed: true },
+  { value: 0, fixed: false },
+  { value: 7, fixed: true },
+  { value: 0, fixed: false },
+  { value: 6, fixed: true },
+];
+
 describe('getRowFromIndex', () => {
   test('returns the corresponding row', () => {
-    const idx = _.random(0, 8);
-    const row = sudoku.rows[idx];
-    expect(getRowFromIndex(idx, 0, sudoku.rows)).toStrictEqual(row);
-    expect(getRowFromIndex(idx, 0, sudoku.rows)).not.toBe(row);
+    expect(getRowFromIndex(0, 0, sudoku)).toStrictEqual(row);
+    expect(getRowFromIndex(0, 0, sudoku)).not.toBe(row);
   });
 });
 
 describe('getColumnFromIndex', () => {
   test('returns the corresponding column', () => {
-    const idx = _.random(0, 8);
-    const column = sudoku.columns[idx];
-    expect(getColumnFromIndex(0, idx, sudoku.rows)).toStrictEqual(column);
-    expect(getColumnFromIndex(0, idx, sudoku.rows)).not.toBe(column);
+    expect(getColumnFromIndex(0, 0, sudoku)).toStrictEqual(column);
+    expect(getColumnFromIndex(0, 0, sudoku)).not.toBe(column);
   });
 });
 
 describe('getBlockFromIndex', () => {
   test('returns the corresponding block', () => {
-    const rowIdx = _.random(0, 8);
-    const colIdx = _.random(0, 8);
-    const blockIdx = rowIdx - (rowIdx % 3) + Math.floor(colIdx / 3);
-    const block = sudoku.blocks[blockIdx];
-    expect(getBlockFromIndex(rowIdx, colIdx, sudoku.rows)).toStrictEqual(block);
-    expect(getBlockFromIndex(rowIdx, colIdx, sudoku.rows)).not.toBe(block);
+    expect(getBlockFromIndex(0, 0, sudoku)).toStrictEqual(block);
+    expect(getBlockFromIndex(0, 0, sudoku)).not.toBe(block);
   });
 });
